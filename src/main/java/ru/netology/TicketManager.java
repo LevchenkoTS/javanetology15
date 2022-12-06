bf1b6b19a3e7caabc1df55dca8f25ce8da972de4
Tatiana Levchenko <medvedovskaya.tatiana@gmail.com>
2022-12-06 03:26:46 +0300

    hw15.2

--- src/main/java/ru/netology/TicketManager.java
+++ src/main/java/ru/netology/TicketManager.java
@@ -1,6 +1,7 @@
 package ru.netology;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class TicketManager {
 
@@ -25,6 +26,21 @@ public class TicketManager {
             }
         }
         Arrays.sort(result);
+        return result;
+    }
+    public Ticket[] findAll(String from, String to, Comparator<Ticket> comparator) {
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket : repo.findAll()) {
+            if (matches(ticket, from, to)) {
+                Ticket[] tmp = new Ticket[result.length +1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[result.length] = ticket;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result, comparator);
         return result;
     }
 
